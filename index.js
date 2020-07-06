f06629939af9d190d4325dc88dc49bd9f18ab462
Madison McCardle <madison.mccardle@powerhrg.com>
2020-07-06 12:01:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+//const names = ["Ada", "Brendan", "Ali"]
+//const type = "birthday"
+function writeCards(names, type) {
+  let thankYous = []
+  for (let i = 0; i < names.length; i++) {
+    thankYous.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`)
+  }
+  return thankYous
+}
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}
